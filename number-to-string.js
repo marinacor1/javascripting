dcd2ab75c4440c94b73c19b351d758a828822f9c
marinacor1 <marinaspam1234@gmail.com>
2016-06-20 10:59:44 -0600

    All programs complete

---
+++ number-to-string.js
@@ -0,0 +1,5 @@
+var n = 128;
+
+var result = n.toString();
+
+console.log(result);
---
+++ revising-strings.js
@@ -0,0 +1,5 @@
+var pizza = 'pizza is alright'
+
+var result = pizza.replace('alright', 'wonderful')
+
+console.log(result);
